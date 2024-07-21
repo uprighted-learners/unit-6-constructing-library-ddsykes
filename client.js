50997c8c0c63f085c2f5187af29e7b0dbbd84891
ddsykes <ddsykes@hotmail.com>
2024-07-21 17:52:43 -0500

    adding the construcitng library lab

--- client.js
+++ client.js
@@ -1,1 +1,51 @@
 // Your code goes here
+
+const { ObjectId } = require("mongodb");
+const Library = require("./library");
+const collection = new Library("mongodb://127.0.0.1:27017", "library", "books");
+collection.test();
+
+async collection(){
+    const client = await this.client()
+    const db = client.db(this.dbName)
+    const collection = db.collection(this.collName);
+    return collection;
+}
+
+async allBooks(){
+    const collection = await this.collection();
+    return collection.find({}).toArray();
+}
+async findOneBook(id){
+    const docId = new ObjectId(id);
+    const collection = await this.collection();
+    return collection.findOne({_id:docId});
+}
+async findManyBooks(query){
+    const collection = await this.collection();
+    return collection.find(query).toArray();
+}
+async addBook(info){
+    const collection = await this.collection();
+    await collection.insertOne(info);
+    console.log("Book was successfull added");
+}
+async changeBook(id, newInfo){
+    const mongoId = new ObjectId(id);
+    const infoObj ={$set: newInfo};
+    const collection = await this.collection();
+    await collection.update({_id: mongoId}, infoObj);
+    console.log("Book updated successfully");
+}
+
+(asyc() =>{
+   const Library = require('./library');
+   const collection = new Library('mongodb://127.0.0.1:27017", "library", "books')
+   await collection.addBook({title: "New Book", author:"Joe Doe", copies: 5});
+   const allBooks = await collection.allBooks();
+   allBooks.forEach(book => console.log(book));
+   const oneBook = await collection.findManyBooks({author:"John Doe"});
+   manyBooks.forEach(book => console.log(book));
+   await collection.changeBook('specific_book_id_here', {title:"Updated Title"});
+   await collection.removeBook('specific_book_id_here') 
+})
--- library.js
+++ library.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const { MongoClient, ObjectId } = require("mongodb");
+
+class Library {
+  constructor(dbUrl, dbName, collName) {
+    this.dbUrl = dbUrl;
+    this.dbName = dbName;
+    this.collName = collName;
+    this.dbclient = null;
+  }
+
+  async client() {
+    console.log(`Connecting to the${this.dbUrl}...`);
+    this.dbclient = await MongoClient.conect(this.dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to the database");
+    return this.dbclient;
+  }
+  async test() {
+    const client = await this.client();
+    client.close();
+  }
+}
